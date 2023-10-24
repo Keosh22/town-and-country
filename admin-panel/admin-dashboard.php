4f8fb59240baf54c8351168cf62d0fad417dcc92
Ken Joshua Rivera Buenavides <kenjoshuabuenavides@gmail.com>
2023-10-24 18:20:34 +0700

    Sidebar Dropdown

--- admin-panel/admin-dashboard.php
+++ admin-panel/admin-dashboard.php
@@ -21,28 +21,94 @@ include("../includes/header.php");
             </div>
           </li>
 
-          <!----------- Dropdown sidebar/Homeowners ---------->
+          <!----------- Dropdown sidebar ---------->
           <li class="sidebar-item">
+            <!-- Homeowners dropdown -->
             <div class="d-grid">
-              <button class="btn btn-dashboard-dropdown">
-                <a href="#" class="sidebar-link sidebar-dropdown-btn" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-dropdown-homeowners"><i class="bx bxs-home"></i>Homeowners<i class="bx bxs-chevron-up"></i></a>
+              <button class="btn btn-dashboard-dropdown btn-homeowners">
+                <a href="#" class="sidebar-link collapsed homeowners-btn" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-dropdown-homeowners"><i class="bx bxs-home"></i>Homeowners</i></a>
               </button>
             </div>
           </li>
-          <ul id="sidebar-dropdown-homeowners" class="sidebar-dropdown collapse list-unstyled">
-            <li>
+          <ul id="sidebar-dropdown-homeowners" class="sidebar-dropdown collapse list-unstyled" data-bs-parent="#sidebar">
+            <li class="sidebar-item">
               <div class="d-grid">
                 <button class="btn btn-dropdown">
-                  <a href="#" class="sidebar-link-dropdown">Members</a>
+                  <a href="#" class="sidebar-link-dropdown">Homeowners List</a>
                 </button>
+              </div>
+            </li>
+            <li class="sidebar-item">
+              <div class="d-grid">
                 <button class="btn btn-dropdown">
-                  <a href="#" class="sidebar-link-dropdown">Non-members</a>
+                  <a href="#" class="sidebar-link-dropdown">Register Accounts</a>
+                </button>
+              </div>
+            </li>
+            <li class="sidebar-item">
+              <div class="d-grid">
+                <button class="btn btn-dropdown">
+                  <a href="#" class="sidebar-link-dropdown">Send Email</a>
+                </button>
+              </div>
+            </li>
+            <li class="sidebar-item">
+              <div class="d-grid">
+                <button class="btn btn-dropdown">
+                  <a href="#" class="sidebar-link-dropdown">Schedule Meeting</a>
                 </button>
               </div>
             </li>
           </ul>
+          <!-- Payments Dropdown -->
+          <li class="sidebar-item">
+            <div class="d-grid">
+              <button class="btn btn-dashboard-dropdown">
+                <a href="#" class="sidebar-link collapsed sidebar-dropdown-btn" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-dropdown-payments"><i class="bx bxs-dollar-circle"></i>Payments</a>
+              </button>
+            </div>
+          </li>
+          <li class="sidebar-item">
+            <ul class="sidebar-dropdown collapse list-unstyled" id="sidebar-dropdown-payments" data-bs-parent="#sidebar">
+              <li class="sidebar-item">
+                <div class="d-grid">
+                  <button class="btn btn-dropdown">
+                    <a href="#" class="sidebar-link-dropdown">Monthly Dues</a>
+                  </button>
+                </div>
+              </li>
+              <li class="sidebar-item">
+                <div class="d-grid">
+                  <button class="btn btn-dropdown">
+                    <a href="#" class="sidebar-link-dropdown">Membership Fee</a>
+                  </button>
+                </div>
+              </li>
+              <li class="sidebar-item">
+                <div class="d-grid">
+                  <button class="btn btn-dropdown">
+                    <a href="#" class="sidebar-link-dropdown">Construction Bond</a>
+                  </button>
+                </div>
+              </li>
+              <li class="sidebar-item">
+                <div class="d-grid">
+                  <button class="btn btn-dropdown">
+                    <a href="#" class="sidebar-link-dropdown">Construction Clearance</a>
+                  </button>
+                </div>
+              </li>
+              <li class="sidebar-item">
+                <div class="d-grid">
+                  <button class="btn btn-dropdown">
+                    <a href="#" class="sidebar-link-dropdown">Material Delivery</a>
+                  </button>
+                </div>
+              </li>
+            </ul>
 
-          <!-- --------- Dropdown sidebar/Payments --------
+
+            <!-- --------- Dropdown sidebar/Payments --------
           <li class="sidebar-item">
             <div class="d-grid">
               <button class="btn btn-dashboard-dropdown">
@@ -68,7 +134,7 @@ include("../includes/header.php");
 
 
 
-          <!-- <div class="accordion accordion-flush" id="accordionParent">
+            <!-- <div class="accordion accordion-flush" id="accordionParent">
             <div class="accordion-item">
               <h2 class="accordion-header">
                 <button class="accordion-button  collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionCollapse">
@@ -104,6 +170,218 @@ include("../includes/header.php");
           <i class="bx bx-menu"></i>
         </button>
       </div>
+
+
+      <!-- ------------SAMPLE TABLE ------------ -->
+      <section class="intro">
+        <div class="bg-image h-100" style="background-image: url('https://mdbootstrap.com/img/Photos/new-templates/tables/img4.jpg');">
+          <div class="mask d-flex align-items-center h-100" style="background-color: rgba(25, 185, 234,.25);">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-12">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="table-responsive">
+                        <table class="table table-hover mb-0">
+                          <thead>
+                            <tr>
+                              <th scope="col"></th>
+                              <th scope="col">Product Detail Views</th>
+                              <th scope="col">Unique Purchases</th>
+                              <th scope="col">Quantity</th>
+                              <th scope="col">Product Revenue</th>
+                              <th scope="col">Avg. Price</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">Value</th>
+                              <td>18,492</td>
+                              <td>228</td>
+                              <td>350</td>
+                              <td>$4,787.64</td>
+                              <td>$13.68</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">Percentage change</th>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-48.8%%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>14.0%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>46.4%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>29.6%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-11.5%</span>
+                                </span>
+                              </td>
+                            </tr>
+                            <tr>
+                              <th scope="row">Average</th>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-17,654</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>28</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>111</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>$1,092.72</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>$-1.78</span>
+                                </span>
+                              </td>
+                            </tr>
+                            <tr>
+                              <th scope="row">Buy-to-details</th>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-48.8%%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>14.0%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>46.4%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>29.6%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-11.5%</span>
+                                </span>
+                              </td>
+                            </tr>
+                            <tr>
+                              <th scope="row">Sales</th>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-17,654</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>28</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>111</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>$1,092.72</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>$-1.78</span>
+                                </span>
+                              </td>
+                            </tr>
+                            <tr>
+                              <th scope="row">Website traffic</th>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-48.8%%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>14.0%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>46.4%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>29.6%</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-11.5%</span>
+                                </span>
+                              </td>
+                            </tr>
+                            <tr>
+                              <th scope="row">Clickthrough</th>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>-17,654</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>28</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>111</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-success">
+                                  <i class="fas fa-caret-up me-1"></i><span>$1,092.72</span>
+                                </span>
+                              </td>
+                              <td>
+                                <span class="text-danger">
+                                  <i class="fas fa-caret-down me-1"></i><span>$-1.78</span>
+                                </span>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- ----------------------SAMPLE TABLE------------------------------ -->
     </div>
   </div>
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
 // Side bar collapse button
 const chevronsElement = document.getElementById("chevron-right");
-
 const sidebarBtnToggler = document.getElementById("sidebar-toggler-btn");
 
 sidebarBtnToggler.addEventListener('click', () => {
   rotateChevrons();
 });
-
 
 function rotateChevrons(){
   chevronsElement.classList.toggle("sidebar-toggler-left");
@@ -15,10 +13,17 @@ function rotateChevrons(){
 
 
 // Sidebar dropwdown chevron
-$(document).ready(function() {
-  $(".sidebar-dropdown-btn").on('click', function(){
-    $(".bxs-chevron-up").toggleClass("rotate-chevron-up");
-    $(".btn-dashboard-dropdown").toggleClass("btn-dashboard-toggle");
-    $(".sidebar-dropdown").toggleClass("sidebar-dropdown-toggle");
-  });
-});
+// $(document).ready(function() {
+  
+//   $(".homeowners-btn").on('click', function(){
+      
+//     $(".btn-dashboard-dropdown").toggleClass("btn-dashboard-toggle");
+//     $(".sidebar-dropdown").toggleClass("sidebar-dropdown-toggle");
+
+   
+//   });
+//   //    $(".bxs-chevron-up").toggleClass("rotate-chevron-up");
+//   //   $(".btn-dashboard-dropdown").toggleClass("btn-dashboard-toggle");
+//   //   $(".sidebar-dropdown").toggleClass("sidebar-dropdown-toggle");
+//   // });
+// });
