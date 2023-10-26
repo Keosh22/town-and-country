9b08d7611c62c4ef1ee509bc4ae81d28f88fb13b
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2023-10-26 15:14:35 +0800

    Add Admin Login and Signup
    Create new folder for admin-login and admin-signup
    Add new CSS files
    
    Note
       The login and signup still has no functionality

---
+++ admin-login/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles/login.css">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+        <div class="row main-row">
+
+            <div class="col-sm-8 left-side d-flex flex-column align-items-center justify-content-center ">
+                <img src="/img/logo.png" class="img-fluid" alt="">
+                <h3 class="header-text text-center mb-5 title" >Town and Country Heights Subdivision</h3>
+            </div>
+
+            <div class="col-sm-4 right-side d-flex flex-column align-items-center justify-content-center justify-content-lg-center">
+               <div class="row">
+                <div class="header-text text-center mb-5">
+                    <img src="login.png" class="img-fluid"  alt="">
+                    <h1>WELCOME</h1>
+                 
+                </div>
+               </div>
+
+               <div class="input-group d-flex justify-content-center text-center">
+                    <form>
+          
+                        <div class="form-group">
+                            
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                   
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+
+                        
+                        <button type="submit" class="btn btn-primary">LOGIN</button>
+
+
+                        <div class="row">
+                            
+                            <!-- Simple link -->
+                            <a href="#!">Forgot password?</a>
+                              
+                        </div>
+                    </form>
+               </div>
+
+
+
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ admin-signup/signup.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles/signup.css">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+        <div class="row main-row">
+
+            <div class="col-md-6 col-lg-8 col-xs-12 left-side d-flex flex-column align-items-center justify-content-center ">
+                <img src="/img/logo.png">
+                <h3 class="header-text text-center mb-5 title" >Town and Country Heights Subdivision</h3>
+            </div>
+
+            <div class="col-md-6 col-lg-4 col-xs-12 right-side d-flex flex-column align-items-center justify-content-center justify-content-lg-center">
+               <div class="row">
+                <div class="header-text text-center mb-1">
+                    <img src="login.png" class="img-fluid"  alt="">
+                    <h1>WELCOME</h1>
+                 
+                </div>
+               </div>
+
+               <div class="input-group d-flex justify-content-center ">
+                    
+                    <form action="" class="">
+                        <div class="group gap-1">
+                            <p class="fs-6 fw-lighter mb-1">Personal Information</p>   
+                            <div class="row">
+                                          
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                                    <input type="text" class="form-control" id="firstname" aria-describedby="emailHelp" placeholder="Firstname">
+                                </div>
+    
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                                    <input type="text" class="form-control" id="lastname" aria-describedby="emailHelp" placeholder="Lastname">
+                                </div>
+    
+                                <div class="col-xs-12 mb-3">
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email">
+                                </div>
+                            </div>
+    
+                        </div>
+                        
+                        <div class="group">
+                            <p class="fs-6 fw-lighter mb-1">Address</p>   
+
+                            <div class="row">
+                                                    
+                                <div class="col-12 col-xs-12 mb-3">
+                                    <input type="text" class="form-control" id="address-line-1" aria-describedby="" placeholder="Street Address">
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                                    <input type="text" class="form-control" id="Phase" aria-describedby="" placeholder="Phase">
+                                </div>
+
+                                <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+                                    <input type="text" class="form-control" id="city" aria-describedby="" placeholder="City">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="group">
+                            <p class="fs-6 fw-lighter lh-lg mb-1">Create account</p>   
+
+                            <div class="row">
+                                                    
+                                <div class="col-12 col-xs-12">
+                                    <input type="text" class="form-control" id="username" aria-describedby="" placeholder="Username">
+                                </div>
+
+                                <div class="col-12 col-xs-12 mt-3">
+                                    <input type="text" class="form-control" id="pasword" aria-describedby="" placeholder="Password">
+                                </div>
+
+                                <div class="ccol-12 col-xs-12 mt-3">
+                                    <input type="text" class="form-control" id="confirm-password" aria-describedby="" placeholder="Confirm Password">
+                                </div>
+                            </div>
+                            
+                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 mt-3">
+                            <button type="button" class="btn btn-primary btn-lg">SIGNUP</button>
+                            </div>
+
+                        
+                            
+                        </div>
+                        </div>
+
+                    </form>
+               </div>
+
+
+
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
