e5118299a442430cddc348e66d75d81f901e0348
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2023-11-10 23:22:41 +0800

    User home page

---
+++ user-panel/home.php
@@ -0,0 +1,58 @@
+<?php
+require "../includes/user-header.php";
+?>
+
+  <main>
+    <div class="container-fluid d-flex flex-column justify-content-center">
+      <div class="row row-logo">
+        <div class="col-sm-12 main-logo d-flex justify-content-center">
+          <img src="/img/logo.png" alt="">
+        </div>
+      </div>
+
+      <div class="row row-title mb-5">
+        <div class="col-sm-12 col-title text-center">
+          <h1>TOWN AND COUNTRY HEIGHTS EXECUTIVE VILLAGE</h1>
+          <p>What would you like to do?</p>
+        </div>
+      </div>
+    
+      <div class="row  justify-content-center gap-1 ">
+        
+        <div class="col-xl-3 col-md-4 features transaction d-flex flex-column align-items-center justify-content-center  text-center" >
+          <i class="far fa-file-lines"style="color: white;" ></i>
+          <p>TRANSACTION</p>
+        </div>
+
+        
+        <div class="col-xl-3 col-md-4 features d-flex flex-column align-items-center justify-content-center text-center">
+          <i class="fa-solid fa-bullhorn" style="color: white;" ></i>
+          
+          <p>ANNOUNCEMENT</p>
+        </div>
+
+        
+        <div class="col-xl-3 col-md-4 features d-flex flex-column align-items-center justify-content-center text-center">
+          <i class="fa-solid fa-bell-concierge" style="color: white;" ></i>
+          
+          <p>SERVICES</p>
+        </div>
+
+
+        <div class="col-xl-3 col-md-4 features d-flex flex-column align-items-center justify-content-center text-center">
+          <i class="fa-regular fa-user" style="color: white;"></i>
+          <p>PROFILE</p>
+        </div>
+
+        <div class="col-xl-3 col-md-4 features d-flex flex-column align-items-center justify-content-center text-center">
+          <i class="fa-regular fa-user" style="color: white;"></i>
+          <p>PROFILE</p>
+        </div>
+
+      </div>
+    </div>
+  </main>
+
+
+</body>
+</html>
