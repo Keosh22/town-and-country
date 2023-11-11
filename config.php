a21f43bd16d9da3013d8c70317ee524d033d1463
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2023-11-11 22:50:01 +0800

    Add Port to Config

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
   define("USER", "root");
   define("PASS", "");
   define("DBNAME", "town_and_country_db");
+  define("PORT", "3308");
   define("DIR", "../");
   define("PATH", dirname(__FILE__));
   define("BASE_URL", "http://localhost/town-and-country/");
