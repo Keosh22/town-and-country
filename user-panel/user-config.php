e39c7d5981a12e797672e5332587b7ae9de02a88
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2023-11-11 21:33:45 +0800

    Working on User Login Create a seperate config file

---
+++ user-panel/user-config.php
@@ -0,0 +1,10 @@
+<?php
+  define("HOST", "localhost");
+  define("USER", "root");
+  define("PASS", "lesthuuur");
+  define("DBNAME", "tch");
+  define("DIR", "../");
+  define("PATH", dirname(__FILE__));
+  define("BASE_URL", "http://localhost/town-and-country/");
+
+?>
---
+++ user-panel/user-login.php
@@ -0,0 +1,72 @@
+<!-- HEADER -->
+<?php require_once("../includes/header.php"); ?>
+<!-- SERVER -->
+<?php require_once("../libs/server.php"); ?>
+<?php
+$server = new Server; // Open/Close connection
+session_start();
+$server->adminSessionLogin();
+
+
+?>
+<?php
+if(isset($_POST["login_submit"])){
+    
+}
+
+?>
+
+
+<body>
+    <!-- MAIN  -->
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+
+        <div class="row main-row d-flex">
+            <!-- LEFT SIDE -->
+            <!-- title and logo -->
+            <div class="col-xxl-8 col-xl-6 col-l-6 col-md-6 col-sm-0 left-side d-flex flex-column align-items-center justify-content-lg-center justify-content-sm-start ">
+                <img src="<?php echo DIR?>img/logo.png" class="img-fluid" alt="">
+                <h1 class="h1 header-text text-center mb-5 title">Town and Country Heights Subdivision</h1>
+            </div>
+
+            <!-- RIGHT SIDE -->
+            <!-- Input and buttons -->
+            <div class="col-xxl-4 col-xl-6 col-l-6  col-md-6 col-sm-12 right-side d-flex flex-column align-items-center justify-content-sm-center   
+            justify-content-lg-center">
+                <!-- UPPER PART -->
+                <div class="row">
+                    <div class="header-text text-center mb-5">
+                        <img src="<?php echo DIR?>img/login.png" class="login-img " alt="">
+                        <h1 class="h1">WELCOME</h1>
+                    </div>
+                </div>
+
+                <!-- FORMS -->
+                <div class="input-group d-flex justify-content-center text-center">
+                    <form action="index.php" method="post">
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+
+                        <!-- SUBMIT BUTTON -->
+                        <input type="submit" class="login-btn btn btn-primary" name="login_submit" value="Log In"></input>
+
+                        <!-- FORGOT PASSWORD -->
+                        <div class="row forgot-password">
+                            <a href="#!">Forgot password?</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <?php
+    include(DIR . "includes/footer.php");
+    ?>
