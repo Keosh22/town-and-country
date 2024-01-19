f655a4c0741a00e3dabddd09711aea7fd40e6354
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2024-01-20 00:02:02 +0800

    create new files for profile functionality

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (isset($_POST['login'])) {
   } else {
     $query = "SELECT * FROM homeowners_users WHERE username = :username";
     $data = ["username" =>  $login_Username];
-    $path = "./user/home.php";
+    $path = "home.php";
     $pass = $login_Pass;
     $userserver->userLogin($query, $data, $pass, $path);
 
