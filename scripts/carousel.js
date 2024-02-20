56e2b092495621af6498863b27379298492ae092
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2024-02-20 10:00:13 +0800

    add auto content changes to carousel with timer

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -10,7 +10,8 @@ $(document).ready(function () {
             announcement_list = data;
             announcement_list_length = announcement_list.length;
 
-    
+
+            
 
             // Adjust the appearance of the buttons based on the counter and announcement_list_length
             if (counter == 0) {
@@ -30,28 +31,46 @@ $(document).ready(function () {
             // Update the content of the carousel
             updateCarouselContent();
 
-            // Set up click event handlers for the buttons
+            // automatic update content of the carousel after interval
+            setInterval(function(){
+                updateCarouselContent();
+                counter += 1;
+                if (counter == announcement_list_length) {
+                    counter = 0;
+                }
+            }, 10000)
+
+            
+            // Next button 
             $("#next").on("click", function () {
                 counter += 1;
                 updateCarouselContent();
                 updateButtonAppearance();
+    
 
             });
 
+            // back button
             $("#back").on("click", function () {
                 counter -= 1;
                 updateCarouselContent();
                 updateButtonAppearance();
 
             });
+            
         },
         error: function (xhr, status, error) {
             console.error("Error: " + error);
         },
+
     });
 
+
+
+    
     // Function to update the content of the carousel based on the current counter value
     function updateCarouselContent() {
+
         // Fade out the existing content
         $("#span_about, #span_content, #span_date").fadeOut(400, function() {
             // Update the content with new data
@@ -60,14 +79,16 @@ $(document).ready(function () {
             $("#span_date").text(announcement_list[counter]["date"]);
             $("#span_date_created").text(announcement_list[counter]["date_created"]);
 
-            
+   
+        
             // Fade in the updated content
             $("#span_about, #span_content, #span_date").fadeIn(400);
         });
+        
     }
     
 
-    // Function to update the appearance of the buttons based on the current counter value
+    // Function to update the appearance of the next and previous buttons based on the current counter value
     function updateButtonAppearance() {
         if (counter == 0) {
             $("#back").css({
@@ -93,4 +114,7 @@ $(document).ready(function () {
             });
         }
     }
+
+
+    
 });
