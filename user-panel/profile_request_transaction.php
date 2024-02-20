dc5f3b39c74cd9606b88d76ccf035d533084f1a9
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2024-02-20 20:57:45 +0800

    edit request transaction

--- user-panel/profile_request_transaction.php
+++ user-panel/profile_request_transaction.php
@@ -52,7 +52,8 @@
       <div class="modal-footer">
 
         <button type="button" class="btn btn-secondary closeBtn" data-dismiss="modal">Close</button>
-        <input type="submit" class="btn btn-primary" value="Request" id="request"></button>
+        <input type="submit" class="btn btn-primary" value="Print" id="request"></button>
+        <input type="submit" class="btn btn-primary" value="Download" id="download"></button>
 
 
 
@@ -66,144 +67,138 @@
 
 <script>
   $(document).ready(function() {
-    //'$("#request").prop("disabled", true);
+  //'$("#request").prop("disabled", true);
 
-    //dates
-    var highest_date;
-    var lowest_date;
+  //dates
+  var highest_date;
+  var lowest_date;
 
-    var lowestYear;
-    var highestYear;
+  var lowestYear;
+  var highestYear;
 
-    var highestMonth;
-    var lowestMonth;
+  var highestMonth;
+  var lowestMonth;
 
-    //data
+  //data
 
-    var transaction_number;
-    var user_id;
-    var category;
-    var month_date;
-    var transaction_date;
-    var firstname;
-    var lastname;
-    var middle_initial;
-    var blk;
-    var lot;
-    var street;
-    var phase;
-    var transaction_number;
-
-
-    var startDate;
-    var endDate;
+  var transaction_number;
+  var user_id;
+  var category;
+  var month_date;
+  var transaction_date;
+  var firstname;
+  var lastname;
+  var middle_initial;
+  var blk;
+  var lot;
+  var street;
+  var phase;
+  var transaction_number;
 
 
-    $.ajax({
-
-      url: '../user-panel/profile_validate_date.php',
-      type: "GET",
-      dataType: "json",
-      success: function(data) {
-        // date_response = response.date_range
-        // payment_response = response.payment_data
-        //date
-        highest_date = data.max_year_month;
-        lowest_date = data.min_year_month;
-
-        //personal data
-
-        //name
-        //     firstname = payment_response.firstname;
-        //     lastname = payment_response.lastname;
-        //     middle_initial = payment_response.middle_initial;
-        //     blk = payment_response.blk;
-        //     lot = payment_response.lot;
-        //     street = payment_response.street;
+  var startDate;
+  var endDate;
 
 
-        //     var name = lastname + "," + firstname + ", " + middle_initial + ".";
-        // var address = blk + " " + lot + "" + street + "" + phase;
+  $.ajax({
+
+    url: '../user-panel/profile_validate_date.php',
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+      // date_response = response.date_range
+      // payment_response = response.payment_data
+      //date
+      highest_date = data.max_year_month;
+      lowest_date = data.min_year_month;
+
+      //personal data
+
+      //name
+      //     firstname = payment_response.firstname;
+      //     lastname = payment_response.lastname;
+      //     middle_initial = payment_response.middle_initial;
+      //     blk = payment_response.blk;
+      //     lot = payment_response.lot;
+      //     street = payment_response.street;
 
 
-        // highest_date_split = highest_date.split("-")
-        // highestYear = highest_date_split[0];
-        // highestMonth = highest_date_split[1];
-
-        // lowest_date_split = lowest_date.split("-");
-        // lowestYear = lowest_date_split[0];
-        // lowestMonth = lowest_date_split[1];
-      },
-
-      error: function(error) {
-        console.log("Error fetching data: " + JSON.stringify(error));
-      }
-    });
-
-    $(function() {
-      $("#request").on("click", function() {
-        $(".message_result").empty();
-
-        startDate = $("#start_date").val();
-        // var start_date_parts = startDate.split("/")
-        // var startMonth = start_date_parts[0];
-        // var startYear = start_date_parts[1];
-
-        endDate = $("#end_date").val();
-        // var end_date_parts = endDate.split("/")
-        // var endMonth = end_date_parts[0];
-        // var endYear = end_date_parts[1];
-        $(".message_result").append("<h4>" + startDate + "/" + highest_date + "</h4>");
+      //     var name = lastname + "," + firstname + ", " + middle_initial + ".";
+      // var address = blk + " " + lot + "" + street + "" + phase;
 
 
+      // highest_date_split = highest_date.split("-")
+      // highestYear = highest_date_split[0];
+      // highestMonth = highest_date_split[1];
 
-        if (
-          ((startDate >= lowest_date && startDate <= highest_date) &&
-            (endDate <= highest_date && endDate >= lowest_date))) {
-          $(".message_result").append("<h1>Proceed</h1>");
-          var url = '../user-panel/receipt.php?start_date=' + startDate + '&end_date=' + endDate;
+      // lowest_date_split = lowest_date.split("-");
+      // lowestYear = lowest_date_split[0];
+      // lowestMonth = lowest_date_split[1];
+    },
 
+    error: function(error) {
+      console.log("Error fetching data: " + JSON.stringify(error));
+    }
+  });
 
-          var newWindow = window.open(url, "_blank");
+  $(function() {
+    $("#request").on("click", function() {
+      $(".message_result").empty();
 
-          newWindow.onload = function() {
-            newWindow.print();
-          };
-          // window.open('../user-panel/receipt.php?name=' + startDate , "_blank", 'width=900,height=600');
-          // $.ajax({
+      startDate = $("#start_date").val();
+      // var start_date_parts = startDate.split("/")
+      // var startMonth = start_date_parts[0];
+      // var startYear = start_date_parts[1];
 
-          //   url: '../user-panel/receipt.php',
-          //   type: "POST",
-          //   dataType: "json",
-
-
-          //   success: function(data) {
-          //     var result = data
-          //       //window.open("../user-panel/receipt.php");
-          //       $(".message_result").append("<h4>" + result +"</h4>");
+      endDate = $("#end_date").val();
+      // var end_date_parts = endDate.split("/")
+      // var endMonth = end_date_parts[0];
+      // var endYear = end_date_parts[1];
+      $(".message_result").append("<h4>" + startDate + "/" + highest_date + "</h4>");
 
 
 
-
-          //   },
-
-          //   error: function(error) {
-          //     console.log("Error fetching data: " + JSON.stringify(error));
-          //   }
-          // });
+      if (
+        ((startDate >= lowest_date && startDate <= highest_date) &&
+          (endDate <= highest_date && endDate >= lowest_date))) {
+        $(".message_result").append("<h1>Proceed</h1>");
+        var url = '../user-panel/receipt.php?start_date=' + startDate + '&end_date=' + endDate;
 
 
+        var newWindow = window.open(url, "_blank");
 
-
-        } else {
-          $(".message_result").append("<h1>There is no transaction on this date</h1>");
-
+        newWindow.onload = function() {
+          newWindow.printThis();
         };
 
+      } else {
+        $(".message_result").append("<h1>There is no transaction on this date</h1>");
 
-      });
+      };
     });
+
+
   });
+
+
+  $("#download").on("click", function() {
+  
+
+    // Make an HTTP request to fetch the content of receipt.php
+    $.get(url, function(data) {
+        // After receiving the content, convert it to PDF
+        html2canvas(data).then(function(canvas) {
+            var imgData = canvas.toDataURL('image/png');
+            var pdf = new jsPDF();
+            var imgWidth = pdf.internal.pageSize.getWidth();
+            var imgHeight = canvas.height * imgWidth / canvas.width;
+            pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+            pdf.save('receipt.pdf');
+        });
+    });
+});
+
+})
 </script>
 
 
@@ -270,7 +265,8 @@
 <script>
 
 </script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.3/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
