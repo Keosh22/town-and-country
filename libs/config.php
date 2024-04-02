4ef9f20bedde7c0056bb1d4305dab67352073f06
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2024-04-02 12:21:53 +0800

    Fix the UI of the carousel

--- libs/config.php
+++ libs/config.php
@@ -1,20 +1,20 @@
 <?php
-  // //Config ni Boss ther
-    //define("LESDBNAME", "town_and_country_db");
-  // define("LESUSER", "lesthuuur");
-  // define("LESPASS", "lesthuuur");
-  // define("PORT", "3308");
+// //Config ni Boss ther
+//define("LESDBNAME", "town_and_country_db");
+define("LESUSER", "lesthuuur");
+define("LESPASS", "lesthuuur");
+define("PORT", "3308");
 
 
 
 
-  define("HOST", "localhost");
-  define("USER", "root");
-  define("PASS", "");
-  define("DBNAME", "town_and_country_db");
-  define("DIR", "../");
-  define("PATH", dirname(__FILE__));
-  define("BASE_URL", "http://localhost/town-and-country/");
+define("HOST", "localhost");
+// define("USER", "root");
+// define("PASS", "");
+define("DBNAME", "town_and_country_db");
+define("DIR", "../");
+define("PATH", dirname(__FILE__));
+define("BASE_URL", "http://localhost/town-and-country/");
 
 
 // 
@@ -26,4 +26,3 @@
 //   define("DIR", "../");
 //   define("PATH", dirname(__FILE__));
 //   define("BASE_URL", "http://localhost/town-and-country/");
-?>
